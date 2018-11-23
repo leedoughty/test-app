033647af20325c04bf248cf41a8d01378de66bc7
Lee Doughty <leedoughtydesign@gmail.com>
2018-11-23 20:45:50 +0000

    Add state for editing, add form function

--- src/Note.js
+++ src/Note.js
@@ -3,15 +3,32 @@ import React, { Component } from 'react'
 class Note extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      editing: false
+    }
     this.edit = this.edit.bind(this)
     this.remove = this.remove.bind(this)
   }
   edit() {
-    alert('editing note')
+    this.setState({
+      editing: true
+    })
   }
   remove() {
     alert('removing note')
   }
+
+  renderForm() {
+    return (
+      <div className="note">
+        <form>
+          <textarea />
+          <button>Save</button>
+        </form>
+      </div>
+    )
+  }
+
   render() {
     return (
       <div className="note">
