bd40e10b11e523b1402037cf6175ffa7fd3c97bb
Lee Doughty <leedoughtydesign@gmail.com>
2018-11-23 20:36:26 +0000

    Bind edit and remove buttons in the constructor

--- src/Note.js
+++ src/Note.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
 
 class Note extends Component {
+  constructor(props) {
+    super(props)
+    this.edit = this.edit.bind(this)
+    this.remove = this.remove.bind(this)
+  }
+  edit() {
+    alert('editing note')
+  }
+  remove() {
+    alert('removing note')
+  }
   render() {
     return (
       <div className="note">
         <p>this is a test note</p>
         <span>
-          <button>Edit</button>
-          <button>Remove</button>
+          <button onClick={this.edit} id="edit">Edit</button>
+          <button onClick={this.remove} id="remove">Remove</button>
         </span>
       </div>
     )
