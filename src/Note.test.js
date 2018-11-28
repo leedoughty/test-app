4dfc78e3af4989d037091049dc54d719d5e30184
Lee Doughty <leedoughtydesign@gmail.com>
2018-11-28 20:06:46 +0000

    Test rendering of note with default text

--- src/Note.test.js
+++ src/Note.test.js
@@ -1,1 +1,10 @@
 import React from 'react'
+import Note from './Note'
+import {mount, shallow} from 'enzyme'
+
+describe('<Note />', () => {
+  it('render default note with New Note', () => {
+    const wrapper = mount(<Note note=''>New Note</Note>);
+    expect(wrapper.find('p').text()).toBe('New Note');
+  });
+});
