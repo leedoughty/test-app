af5255282d2c6f1d4b65936bab473b8abed26985
Lee Doughty <leedoughtydesign@gmail.com>
2018-11-28 20:47:15 +0000

    Test random between function

--- src/Note.test.js
+++ src/Note.test.js
@@ -7,4 +7,9 @@ describe('<Note />', () => {
     const wrapper = mount(<Note note=''>New Note</Note>);
     expect(wrapper.find('p').text()).toBe('New Note');
   });
+  it('places note in random location', () => {
+    const firstNote = mount(<Note >New Note</Note>);
+    const secondNote = mount(<Note >New Note</Note>);
+    expect(firstNote.find('.note').prop('style')).not.toBe(secondNote.find('.note').prop('style'))
+  })
 });
